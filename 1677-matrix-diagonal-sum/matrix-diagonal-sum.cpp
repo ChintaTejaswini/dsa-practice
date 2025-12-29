c0c57a68d5939343c4af4a8b52eaad78bb2f9e69
C. Tejaswini <chintatejaswini25@gmail.com>
2025-12-29 18:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (16.10%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int sum = 0; //temp sum
-        //brute force, logic is the diagonal are in the 
-        //index where i=j and i+j=square row or coloumn
-        // tc = O(n**2) due to two for loops   //29/12/2025 learning arrays
+        int sum = 0;
         int n = mat.size();
-        for(int i=0;i<n;i++){     
-            for(int j=0;j<n;j++){
-                if(i==j or i+j==n-1){
-                    sum += mat[i][j];
-                }
+        //optimized solution 
+        // logic the diagonals are in such a way that i=j and i+j=n-1, j=n-1-i
+        //where n is row or coloumn since its a square we could say mat[i][i] and mat[i][n-1-i] but when adding mat[i][n-1-i] the center element might be added again. So in order to avoid that we check if i!= n-1-i then we add that element.
+        //Praticing arrays //29/12/2025
+        for(int i=0;i<n;i++){
+            sum += mat[i][i];
+            if(i!=n-1-i){
+                sum+=mat[i][n-1-i];
             }
         }
         return sum;
